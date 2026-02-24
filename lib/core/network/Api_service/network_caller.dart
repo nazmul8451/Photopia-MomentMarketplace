9aba541a48c3324a0d885466e149dbf2f4aa052a
nazmul8451 <heytrading118@gmail.com>
2026-02-25 04:10:47 +0600

    add network caller

--- lib/core/network/Api_service/network_caller.dart
+++ lib/core/network/Api_service/network_caller.dart
@@ -1,3 +1,5 @@
+import 'package:http/http.dart' ;
+
 class NetworkResponse {
   final bool issSuccess;
   final String? errorMessage;
@@ -10,4 +12,25 @@ class NetworkResponse {
     this.body,
     this.errorMessage,
   });
+}
+
+
+class NetworkCaller{
+
+  static Future<NetworkResponse> getRequest({required String url, String? token}) async {
+
+    try{
+      final Uri uri = Uri.parse(url);
+      final  Map<String,String> headers = {'Accept' :"application/json"};
+
+      final Response response = await get(uri,headers: headers);
+
+
+
+
+    }catch(e){
+
+    }
+
+  }
 }
